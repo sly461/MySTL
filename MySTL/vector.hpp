4a61ee8c72534fc49e7624d7dcb2421f8653da0c
sly461 <857165109@qq.com>
2020-12-27 23:11:14 +0800

    init vector

---
+++ MySTL/vector.hpp
@@ -0,0 +1,18 @@
+/**
+ * vector容器
+**/
+#ifndef _VECTOR_H_
+#define _VECTOR_H_
+
+#include "allocator.hpp"
+
+namespace MySTL {
+    template<class T, class Alloc = allocator<T>>
+    class vector {
+        
+    }
+}
+
+
+
+#endif
