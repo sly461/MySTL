6ac68441f5008621ae61f5577208cd2599d56d75
sly461 <857165109@qq.com>
2020-12-27 14:29:58 +0800

    iterator_category

--- MySTL/iterator.hpp
+++ MySTL/iterator.hpp
@@ -5,7 +5,31 @@
 #define _ITERATOR_H_
 
 
+#include <cstddef>
 
+namespace MySTL{
+    //五种迭代器类型
+    struct input_iterator_tag {};
+    struct output_iterator_tag {};
+    struct forward_iterator_tag: public input_iterator_tag {};
+    struct bidirectional_iterator_tag: public forward_iterator_tag {};
+    struct random_access_iterator_tag: public bidirectional_iterator_tag {};
+
+    //iterator 模板
+    template <class Category, class T, class Distance = ptrdiff_t, 
+              class Pointer = T*, class Reference = T&>
+    struct iterator {
+        using iterator_category = Category;
+        using value_type = T;
+        using difference_type = Distance;
+        using pointer = Pointer;
+        using reference = Reference;
+    };
+
+    //iterator_traits
+    
+
+}
 
 
 #endif
